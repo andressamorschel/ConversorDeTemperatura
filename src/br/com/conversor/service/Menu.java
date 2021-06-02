fe8d81127eb894cbd68e641d087c71eb3d57af28
real <andressamorschel>
2021-06-02 20:26:01 -0300

    feat: implementando loops e tratamento de excessões no menu

--- src/br/com/conversor/service/Menu.java
+++ src/br/com/conversor/service/Menu.java
@@ -3,7 +3,7 @@ package br.com.conversor.service;
 import java.util.Scanner;
 
 public class Menu {
- 
+
 	private CalcularTemperatura calcularCelcius;
 	private CalcularTemperatura calcularFahreinheit;
 
@@ -13,54 +13,76 @@ public class Menu {
 		this.calcularFahreinheit = calcularFahreinheit;
 	}
 
-
-
 	public void opcoes() {
-		System.out.println("===Conversor de Temperatura===");
-		Scanner teclado = new Scanner(System.in); 
-		
-		System.out.println("Selecione uma opção: \n"
-				+ "1- Converter Fahreinheit para Celsius \n"
-				+ "2- Converter Celcius para Fahreinheit \n"
-				+ "3- Sair"); 
-		var resposta = teclado.nextInt();
-		switch(resposta) {
-		case 1 :  
-			calcularCelcius();
-			break;
-		case 2 :
-			calcularFahreinheit();
-			break;
-		case 3 :
-			break;
-		}
-		
-	
-	}
- 
 
+		System.out.println("\n=========================\n" + "Conversor de Temperatura\n" + "=========================");
+		Scanner teclado = new Scanner(System.in);
+
+		try {
+			System.out.println("Selecione uma opção: \n" + "1- Converter Fahreinheit para Celsius \n"
+					+ "2- Converter Celcius para Fahreinheit \n" + "3- Sair");
+
+			var resposta = teclado.nextInt();
+
+			while (resposta != 3) {
+				switch (resposta) {
+				case 1:
+					calcularCelcius();
+					break;
+				case 2:
+					calcularFahreinheit();
+					break;
+				case 3:
+					break;
+				}
+			}
+		} catch (java.util.InputMismatchException e) {
+			System.err.println("Algo deu errado. Tente novamente!");
+			opcoes();
+		}
+
+	}
 
 	public void calcularFahreinheit() {
-		System.out.println("Informe o valor em Celsius: ");
-		Scanner teclado = new Scanner(System.in);
-		Double valorEmCelsius = teclado.nextDouble();
-		Double resultado =  this.calcularFahreinheit.calcular(valorEmCelsius);
-		System.out.println("O resultado é: " + resultado);
-		
+
+		try {
+			System.out.println("Informe o valor em Celsius: ");
+			Scanner teclado = new Scanner(System.in);
+			Double valorEmCelsius = teclado.nextDouble();
+
+			if (valorEmCelsius == 3) {
+				opcoes();
+			}
+			Double resultado = this.calcularFahreinheit.calcular(valorEmCelsius);
+			System.out.println("O resultado é: " + resultado);
+		} catch (java.util.InputMismatchException e) {
+			System.err.println("Algo deu errado. Tente novamente!");
+			opcoes();
+
+		}
+
 	}
-
-
 
 	public void calcularCelcius() {
-		System.out.println("Informe o valor em Fahreinheit: ");
-		Scanner teclado = new Scanner(System.in);
-		Double valorEmFahreinheit = teclado.nextDouble();
-		Double resultado = this.calcularCelcius.calcular(valorEmFahreinheit); 
-		System.out.println("O resultado é: " + resultado);
-		
+
+		try {
+			System.out.println("Informe o valor em Fahreinheit: ");
+			Scanner teclado = new Scanner(System.in);
+			Double valorEmFahreinheit = teclado.nextDouble();
+
+			if (valorEmFahreinheit == 3) {
+				opcoes();
+			}
+
+			Double resultado = this.calcularCelcius.calcular(valorEmFahreinheit);
+			System.out.println("O resultado é: " + resultado);
+
+		} catch (java.util.InputMismatchException e) {
+			System.err.println("Algo deu errado. Tente novamente!");
+			opcoes();
+
+		}
+
 	}
 
-
-
-	
 }
